9a75b83e90273efac988236a9b9a440de9e35ad6
kenneth <kg_azevedo@hotmail.com>
2020-12-12 16:24:29 -0300

    first commit

---
+++ src/main/java/com/unifacef/comics/ComicsApplication.java
@@ -0,0 +1,13 @@
+package com.unifacef.comics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unifacef/comics/ComicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifacef.comics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
