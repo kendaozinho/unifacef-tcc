b542d58b4efbcca0617b02b4fbf0c390545fdc61
kenneth <kg_azevedo@hotmail.com>
2020-12-14 15:00:57 -0300

    fix application version

--- src/main/java/com/unifacef/tcc/util/ApplicationUtil.java
+++ src/main/java/com/unifacef/tcc/util/ApplicationUtil.java
@@ -1,37 +1,15 @@
 package com.unifacef.tcc.util;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Component;
+import com.unifacef.tcc.StartApplication;
 
-import javax.annotation.PostConstruct;
-import java.util.Collections;
-import java.util.Map;
-
-@Component
 public class ApplicationUtil {
-  private static ApplicationContext staticApplicationContext;
-
-  @Autowired
-  private ApplicationContext applicationContext;
-
   public static String getVersion() {
     try {
-      Map<String, Object> beans =
-          ApplicationUtil.staticApplicationContext.getBeansWithAnnotation(SpringBootApplication.class);
-
-      return (beans == null || beans.isEmpty()) ?
-          null : Collections.singletonList(beans.values()).get(0).getClass().getPackage().getImplementationVersion();
+      return StartApplication.class.getPackage().getImplementationVersion();
     } catch (Throwable t) {
       System.err.println("Unable to get application version: " + t.toString());
     }
 
     return null;
-  }
-
-  @PostConstruct
-  public void init() {
-    ApplicationUtil.staticApplicationContext = this.applicationContext;
   }
 }
